e1a87216b483a7a1affde472812784b108ac465d
wald-d <48999686+wald-d@users.noreply.github.com>
2019-11-02 17:25:38 -0700

    Update calculator.js
    Added functions to convert inorder operations to post order operations

--- calculator.js
+++ calculator.js
@@ -1,1 +1,60 @@
 // JavaScript source code
+function Order(operator) {
+    switch (operator) {
+        case "(":
+        case ")":
+            return 4;
+        case "^":
+            return 3;
+        case "*":
+        case "/":
+            return 2;
+        case "+":
+        case "-":
+            return 1;
+        default:
+            //is a number
+            return 0;
+    }
+}
+function PostOrder(expression) {
+    var postFixStack = [];
+    var opStack = [];
+    var number = '';
+
+    for (var i = 0; i < expression.length; i++) {
+        var c = expression.charAt(i);
+        if (!isNaN(parseInt(c)) || c == '.') {
+            number += c;
+        } else {
+            if (number != '') {
+                postFixStack.push(number);
+                number = '';
+            }
+            
+            if (c == ')') {
+                while (opStack[opStack.length - 1] != '(') {
+                    postFixStack.push(opStack.pop());
+                }
+                opStack.pop();
+            } else {
+                while (opStack.length && (Order(c) <= Order(opStack[opStack.length - 1])) && (opStack[opStack.length - 1] != '(')) {
+                    postFixStack.push(opStack.pop());
+                }
+                opStack.push(c);
+            }
+        }
+    }
+
+    if (number != '') {
+        postFixStack.push(number);
+    }
+
+    while (opStack.length) {
+        postFixStack.push(opStack.pop());
+    }
+
+    return postFixStack;
+}
+
+console.log(PostOrder("(1+2*(3-4))/5"));
